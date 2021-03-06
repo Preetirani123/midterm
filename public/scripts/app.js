2c280eebf6166a2de9cff5699405bc8fb9add242
Kyle Lemmon <kylelemmon@Kyles-MacBook-Air.local>
2021-03-06 11:28:22 -0800

    add init. routes for menu, orders, and users

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,6 +3,7 @@ $(() => {
     method: "GET",
     url: "/api/users"
   }).done((users) => {
+    console.log("load users");
     for(user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
