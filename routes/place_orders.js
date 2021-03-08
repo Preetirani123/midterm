4de301368988a17fd77140edf55a0c88e0383030
Kyle Lemmon <kylelemmon@Kyles-MacBook-Air.local>
2021-03-08 00:18:10 -0800

    add more detail to checkout flow

--- routes/place_orders.js
+++ routes/place_orders.js
@@ -9,7 +9,6 @@ module.exports = (db) => {
     const menu_items = req.body.menu_items;
     const values = [user, menu_items[0], menu_items];
 
-
     db.query(`
     INSERT INTO orders (user_id, menu_id, food_items_by_id, order_time)
     VALUES ($1, $2, $3, now())
@@ -17,6 +16,24 @@ module.exports = (db) => {
       .then(data => {
         const order_id = data.rows[0].id;
         res.json(order_id);
+
+
+
+      // ------>
+      // ------> TWILLIO TEXT MSG TO RESTAURANT WILL BE CALLED HERE
+      // ------>
+
+
+        const est_time =  60 * 1000;// temp. test value
+        setTimeout(function(){
+        console.log(`Testing Server Side Timeout`);
+        // ------>
+        // ------> TWILLIO TEXT MSG TO CLIENT WILL BE CALLED HERE, MAYBE?
+        // ------> Not sure if setTimeouts can be used on the server reliably.
+
+          },est_time)
+
+
       })
       .catch(err => {
         res
