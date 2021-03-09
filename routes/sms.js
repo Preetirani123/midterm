3e2eceae3698ef820d72b61a83486ef9ea7b6d75
Liam Butler <lrbutler@gmail.com>
2021-03-09 02:50:39 +0000

    Add SMS forwarding

--- routes/sms.js
+++ routes/sms.js
@@ -2,13 +2,15 @@
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const router  = express.Router();
-console.log("hi")
+require('dotenv').config();
+const phoneNumber = process.env.TWILIO_NUMBER
 
 module.exports = (db) => {
 
 router.post('/', (req, res) => {
   const twiml = new MessagingResponse();
-  twiml.message(`We'll let the diner know. Thanks!`);
+  const message = `Thank you for your order! Your food will be ready in ${req.body.Body} minutes`
+  twiml.message({ to: process.env.USER_NUMBER, from: phoneNumber }, message);
 
   res.writeHead(200, { 'Content-Type': 'text/xml' });
   res.end(twiml.toString());
