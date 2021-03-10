c66994c496a7196e04f3d12a0a7c0e0cb8c07ea5
Kyle Lemmon <kylelemmon@Kyles-MacBook-Air.local>
2021-03-10 12:14:09 -0800

    some small css changes

--- db/seeds/01_menu.sql
+++ db/seeds/01_menu.sql
@@ -1,20 +1,20 @@
 -- Menu table seeds here (Example)
 INSERT INTO menu (nation, food_item, description, price, image_url)
-VALUES ('Italy 🇮🇹','Pizza', 'A savoury dish consisting of round dough topped with tomatoes and cheese', 15, 'https://i.imgur.com/lYjLEUr.jpg'),
-('USA 🇺🇸','Burger', 'You know what a burger is!', 20, 'https://bit.ly/3qoYasw'),
-('Japan 🇯🇵','Sushi', 'Prepared vinegared rice accompanying a variety of ingredients, such as seafood', 30, 'https://i.imgur.com/IBeDA8Y.jpg'),
-('Canada 🇨🇦','Poutine', 'A dish of french fries and cheese curds topped with a brown gravy', 5, 'https://i.imgur.com/h36LiZ9.jpg'),
-('China 🇨🇳','Peking Duck', 'A dish from Beijing that has been prepared since the Imperial era', 100, 'https://i.imgur.com/AmbJynS.jpg'),
-('Georgia 🇬🇪','Khachapuri', 'A traditional Georgian dish of cheese-filled bread', 15, 'https://i.imgur.com/xxaGMFb.jpeg'),
-('Vietnam 🇻🇳','Phở', 'A soup consisting of broth, rice noodles, herbs, and meat.', 10, 'https://i.imgur.com/KJ7PVKl.jpeg'),
-('Mexico 🇲🇽','Taco', 'A traditional Mexican dish consisting of a small hand-sized corn or wheat tortilla topped with a filling', 3, 'https://i.imgur.com/cs94qMu.jpg'),
-('Thailand 🇹🇭','Pad Thai', 'A stir-fried rice noodle dish commonly served as a street food', 13, 'https://i.imgur.com/FZ6MmHu.jpeg'),
-('Ethiopia 🇪🇹','Doro Wat', 'Made from chicken and sometimes hard-boiled eggs, it is the most popular traditional food in Ethiopia', 18, 'https://i.imgur.com/qDvOjhT.jpeg'),
-('Lebanon 🇱🇧','Kibbeh', 'A family of dishes based on spiced ground meat, onions, and grain', 6, 'https://i.imgur.com/vmG3sZD.jpg'),
-('Korea 🇰🇷','Kimchi', 'Spicy fermented cabbage', 2, 'https://i.imgur.com/VR2LNXG.jpg'),
-('Cuba 🇨🇺','Ropa Vieja', 'Shredded or pulled stewed beef with vegetables', 35, 'https://i.imgur.com/4fLs1he.jpeg'),
-('India 🇮🇳','Khichdi ', 'A dish made of rice and lentils.', 27, 'https://i.imgur.com/Ok1WRJx.jpg'),
-('Ukraine 🇺🇦','Borscht', 'A sour soup made with beetroots.', 6, 'https://i.imgur.com/uHOxoiP.jpg');
+VALUES ('🇮🇹','Pizza', 'A savoury dish consisting of round dough topped with tomatoes and cheese', 15, 'https://i.imgur.com/lYjLEUr.jpg'),
+('🇺🇸','Burger', 'You know what a burger is!', 20, 'https://bit.ly/3qoYasw'),
+('🇯🇵','Sushi', 'Prepared vinegared rice accompanying a variety of ingredients, such as seafood', 30, 'https://i.imgur.com/IBeDA8Y.jpg'),
+('🇨🇦','Poutine', 'A dish of french fries and cheese curds topped with a brown gravy', 5, 'https://i.imgur.com/h36LiZ9.jpg'),
+('🇨🇳','Peking Duck', 'A dish from Beijing that has been prepared since the Imperial era', 100, 'https://i.imgur.com/AmbJynS.jpg'),
+('🇬🇪','Khachapuri', 'A traditional Georgian dish of cheese-filled bread', 15, 'https://i.imgur.com/xxaGMFb.jpeg'),
+('🇻🇳','Phở', 'A soup consisting of broth, rice noodles, herbs, and meat.', 10, 'https://i.imgur.com/KJ7PVKl.jpeg'),
+('🇲🇽','Taco', 'A traditional Mexican dish consisting of a small hand-sized corn or wheat tortilla topped with a filling', 3, 'https://i.imgur.com/cs94qMu.jpg'),
+('🇹🇭','Pad Thai', 'A stir-fried rice noodle dish commonly served as a street food', 13, 'https://i.imgur.com/FZ6MmHu.jpeg'),
+('🇪🇹','Doro Wat', 'Made from chicken and sometimes hard-boiled eggs, it is the most popular traditional food in Ethiopia', 18, 'https://i.imgur.com/qDvOjhT.jpeg'),
+('🇱🇧','Kibbeh', 'A family of dishes based on spiced ground meat, onions, and grain', 6, 'https://i.imgur.com/vmG3sZD.jpg'),
+('🇰🇷','Kimchi', 'Spicy fermented cabbage', 2, 'https://i.imgur.com/VR2LNXG.jpg'),
+('🇨🇺','Ropa Vieja', 'Shredded or pulled stewed beef with vegetables', 35, 'https://i.imgur.com/4fLs1he.jpeg'),
+('🇮🇳','Khichdi ', 'A dish made of rice and lentils.', 27, 'https://i.imgur.com/Ok1WRJx.jpg'),
+('🇺🇦','Borscht', 'A sour soup made with beetroots.', 6, 'https://i.imgur.com/uHOxoiP.jpg');
 
 
 
